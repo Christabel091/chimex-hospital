b3be3eea34069286da6a9707175331be148f5c6c
Christabel Obi-Nwosu <157861550+Christabel091@users.noreply.github.com>
2026-08-03 19:58:38 -0700

    Describe your changes

--- src/router.js
+++ src/router.js
@@ -1,56 +1,56 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 // Import pages
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-import Program from './pages/Program.vue'
-import Donate from './pages/Donate.vue'
-import Appointment from './pages/Appointment.vue'
-import Contact from './pages/Contact.vue'
+import Home from "./pages/Home.vue";
+import About from "./pages/About.vue";
+import Program from "./pages/Program.vue";
+import Donate from "./pages/Donate.vue";
+import Appointment from "./pages/Appointment.vue";
+import Contact from "./pages/Contact.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About,
   },
   {
-    path: '/program',
-    name: 'Program',
+    path: "/program",
+    name: "Program",
     component: Program,
   },
   {
-    path: '/donate',
-    name: 'Donate',
+    path: "/donate",
+    name: "Donate",
     component: Donate,
   },
   {
-    path: '/appointment',
-    name: 'Appointment',
+    path: "/appointment",
+    name: "Appointment",
     component: Appointment,
   },
   {
-    path: '/contact',
-    name: 'Contact',
+    path: "/contact",
+    name: "Contact",
     component: Contact,
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { top: 0 }
+      return { top: 0 };
     }
   },
-})
+});
 
-export default router
+export default router;
